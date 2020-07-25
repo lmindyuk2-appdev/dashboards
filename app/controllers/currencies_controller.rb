d81f3e25d30816ba6a99d7b37b0ff59648a484ee
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-07-25 19:37:40 +0000

    2/3 done

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,29 @@
+class CurrenciesController < ActionController::Base
+  def first_currency
+    currencies_url = "https://api.exchangerate.host/symbols"
+    currencies_raw_file = open(currencies_url).read
+    currencies_parsed_file = JSON.parse(currencies_raw_file)
+    @unique_currencies = currencies_parsed_file['symbols'].keys
+    render({:template => "currency_templates/step_one.html.erb"})
+  end
+
+  def second_currency
+    currencies_url = "https://api.exchangerate.host/symbols"
+    currencies_raw_file = open(currencies_url).read
+    currencies_parsed_file = JSON.parse(currencies_raw_file)
+    @unique_currencies = currencies_parsed_file['symbols'].keys
+    @first_currency = params.fetch("first_currency") 
+    render({:template => "currency_templates/step_two.html.erb"})
+  end
+
+  def exchange_rate
+    @first_currency = params.fetch("first_currency") 
+    @second_currency = params.fetch("second_currency") 
+    exchange_rate_url = "https://api.exchangerate.host/convert?from=#{first_currency}&to=#{second_currency}"
+    exchange_rate_raw_file = open(exchange_rate_url).read
+    exchange_rate_parsed_file = JSON.parse(exchange_rate_raw_file)
+    @exchange_rate = exchange_rate_parsed_file['result']
+
+    render({:template => "currency_templates/step_three.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/forex",{:controller => "currencies",:action => "first_currency"})
+  get("/forex/:first_currency",{:controller => "currencies",:action => "second_currency"})
+  get("/forex/:first_currency/:second_currency",{:controller => "currencies",:action => "exchange_rate"})
 end
